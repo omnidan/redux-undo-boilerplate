6470ccfa6e2ec46e287e4d1ca012751efd6b1392
Daniel Bugl <daniel.bugl@touchlay.com>
2015-09-18 01:49:39 +0200

    update for redux-undo 0.2

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,15 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../actions/counter';
+import undoable, { ifAction } from 'redux-undo';
 
 
 const rootReducer = combineReducers({
-  counter
+  counter: undoable(counter, {
+    filter: ifAction([INCREMENT_COUNTER, DECREMENT_COUNTER]),
+    limit: 10,
+    debug: true
+  })
 });
 
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,8 +4,6 @@ import thunkMiddleware from 'redux-thunk';
 import promiseMiddleware from 'redux-promise';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../actions/counter';
-import reduxUndo, { ifAction } from 'redux-undo';
 
 const loggerMiddleware = createLogger({
   level: 'info',
@@ -15,17 +13,10 @@ const loggerMiddleware = createLogger({
 
 let createStoreWithMiddleware;
 
-const reduxUndoConfig = {
-  filter: ifAction([INCREMENT_COUNTER, DECREMENT_COUNTER]),
-  limit: 10,
-  debug: true
-};
-
 if (typeof __DEVTOOLS__ !== 'undefined' && __DEVTOOLS__) {
   const { devTools, persistState } = require('redux-devtools');
   createStoreWithMiddleware = compose(
     applyMiddleware(thunkMiddleware, promiseMiddleware, loggerMiddleware),
-    reduxUndo(reduxUndoConfig),
     devTools(),
     persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
     createStore
@@ -33,7 +24,6 @@ if (typeof __DEVTOOLS__ !== 'undefined' && __DEVTOOLS__) {
 } else {
   createStoreWithMiddleware = compose(
     applyMiddleware(thunkMiddleware, promiseMiddleware),
-    reduxUndo(reduxUndoConfig),
     createStore
   );
 }
